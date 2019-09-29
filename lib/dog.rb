3f6a89a0d53c5f47e0e61db572aba5c2e5d84035
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-29 18:44:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,11 +4,19 @@ class Dog
     @breed = breed
   end
   
-  def breed=(breed="Mutt")
+  
+  def breed=(breed)
     @breed = breed
   end
+  def breed
+    @breed
+  end
+  
   
   def name=(name)
     @name = name
+  end
+  def name 
+    @name
   end
 end
